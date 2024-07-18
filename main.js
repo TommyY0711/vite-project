316f037e6b180c8d5c918c0ee24e347ec5aa1c5c
TommyY0711 <tommyxy07@gmail.com>
2024-07-17 22:08:56 -0400

    changes.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './style.css'  
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 
 document.addEventListener('DOMContentLoaded', function () {
   const cards = document.querySelectorAll('.card');
